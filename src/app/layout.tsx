a2e26ff17da5a6e6a61a9acf3117d3c4185f800a
kamil.szerlag <kamilszerlag@gmail.com>
2023-11-06 16:19:34 +0100

    module-3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { ShoppingCart } from "lucide-react";
-import { ActiveLink } from "@/ui/atoms";
 import { Footer } from "@/ui/molecules/Footer";
 import { Header } from "@/ui/molecules/Header";
 
